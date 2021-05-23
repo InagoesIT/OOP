979edbda2b5e13bc7b2baba3ee16b788b1670bf6
InagoesIT <wenulife@gmail.com>
2021-05-23 14:21:54 +0300

    Testing improved

--- Lab_11/tests/vector_test.cpp
+++ Lab_11/tests/vector_test.cpp
@@ -3,21 +3,7 @@
 
 class VectorTest : public testing::Test {
 protected: 
-    Vector<int> v1; // cazul vid
-    //Vector<int> v2; // un element
-    //Vector<int> v3; // un vector aleator
-    //Vector<int> v4; // un vector descrescator
-    //Vector<int> v5; // un vector crescator
-    //Vector<int> v6; // un vector cu numere negative
-
-    virtual void SetUp() 
-    {
-       // v2.Adauga(3);
-       // v3.Adauga(4); v3.Adauga(200); v3.Adauga(20); v3.Adauga(1); v3.Adauga(55);
-       // v4.Adauga(5); v4.Adauga(4); v4.Adauga(3); v4.Adauga(2); v4.Adauga(1); v4.Adauga(0);
-        //v5.Adauga(0); v5.Adauga(1); v5.Adauga(2); v5.Adauga(3); v5.Adauga(4); v5.Adauga(5);
-        //v6.Adauga(-10); v6.Adauga(-8); v6.Adauga(-9); v6.Adauga(-11);
-    }
+    Vector<int> v1;
 };
 
 TEST_F(VectorTest, SortareVida)
@@ -28,31 +14,35 @@ TEST_F(VectorTest, SortareVida)
 
 TEST_F(VectorTest, Sortare1Elem) 
 {
-    //v1.Adauga(3);
+    v1.Adauga(3);
     v1.Sortare();
     EXPECT_STREQ("3 ", v1.GetArrayString().c_str());
 }
 
-/*TEST_F(VectorTest, SortareObisnuita)
+TEST_F(VectorTest, SortareObisnuita)
 {
-    v3.Sortare();
-    EXPECT_STREQ("1 4 20 55 200 ", v3.GetArrayString().c_str());
+    v1.Adauga(4); v1.Adauga(200); v1.Adauga(20); v1.Adauga(1); v1.Adauga(55);
+    v1.Sortare();
+    EXPECT_STREQ("1 4 20 55 200 ", v1.GetArrayString().c_str());
 }
 
 TEST_F(VectorTest, SortareDescr)
 {
-    v4.Sortare();
-    EXPECT_STREQ("0 1 2 3 4 5 ", v4.GetArrayString().c_str());
+    v1.Adauga(5); v1.Adauga(4); v1.Adauga(3); v1.Adauga(2); v1.Adauga(1); v1.Adauga(0);
+    v1.Sortare();
+    EXPECT_STREQ("0 1 2 3 4 5 ", v1.GetArrayString().c_str());
 }
 
 TEST_F(VectorTest, SortareCresc)
 {
-    v5.Sortare();
-    EXPECT_STREQ("0 1 2 3 4 5 ", v5.GetArrayString().c_str());
+    v1.Adauga(0); v1.Adauga(1); v1.Adauga(2); v1.Adauga(3); v1.Adauga(4); v1.Adauga(5);
+    v1.Sortare();
+    EXPECT_STREQ("0 1 2 3 4 5 ", v1.GetArrayString().c_str());
 }
 
 TEST_F(VectorTest, SortareNegativa)
 {
-    v6.Sortare();
-    EXPECT_STREQ("-11 -10 -9 -8 ", v6.GetArrayString().c_str());
-}*/
+    v1.Adauga(-10); v1.Adauga(-8); v1.Adauga(-9); v1.Adauga(-11);
+    v1.Sortare();
+    EXPECT_STREQ("-11 -10 -9 -8 ", v1.GetArrayString().c_str());
+}
